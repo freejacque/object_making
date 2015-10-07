0a1390d4aef8c41d7e9f0df2701c7762b4a33966
freejacque <freejacque@gmail.com>
2015-10-06 22:49:42 -0400

    created two constructors and two prototypes

---
+++ index.js
@@ -0,0 +1,21 @@
+// This shows inheritance through a chain of prototypes
+
+var Vehicle = function() {};
+
+Vehicle.prototype.drive = function() {
+  console.log('vroom...');
+};
+
+
+var Car = function() {};
+
+Car.prototype = new Vehicle();
+
+Car.prototype.honk = function() {
+  console.log('honk honk');
+};
+
+var myCar = new Car();
+
+myCar.honk();
+myCar.drive();

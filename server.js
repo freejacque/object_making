22640e47f043c3891669d59efbf07230d74cb64b
freejacque <freejacque@gmail.com>
2015-10-08 19:45:32 -0400

    added a for loop to the server to illustrate blocking

--- server.js
+++ server.js
@@ -5,3 +5,11 @@ http.createServer(function(request, response) {
   response.write('<b>Hello World</b>');
   response.end();
 }).listen(8080);
+
+// used to test for blocking
+var a;
+for (var i = 0; i < 1000000000; i += 1) {
+  a = i;
+}
+
+console.log('For loop has finished');

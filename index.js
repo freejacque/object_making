d82ce7bacabd089bcef15069fb1adcd0bda469b3
freejacque <freejacque@gmail.com>
2015-10-07 20:48:00 -0400

    added comments

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ myCar.drive();
 
 
 // This shows inheritance using Object.create()
+// This takes a little more memory than the above method but uses less code
 
 var vehicle = {};
 vehicle.drive = function() {

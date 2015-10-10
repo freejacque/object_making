d775289eba91bfcc47404136c8afe261f5084af6
freejacque <freejacque@gmail.com>
2015-10-09 22:37:43 -0400

    added comments

--- requestHandler.js
+++ requestHandler.js
@@ -2,12 +2,15 @@
 
 var fs = require('fs');
 
+// a ReadStream object is created
 var stream = fs.createReadStream('/file/path');
 
+// a callback is attached to data events
 stream.on('data', function(data) {
   console.log('Received data: ' + data);
 });
 
+//  a callback is attached to the end event
 stream.on('end', function() {
   console.log('End of file has been reached');
 });

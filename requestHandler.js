487be85074c5429a5633e4de87a341df395c5963
freejacque <freejacque@gmail.com>
2015-10-11 21:26:19 -0400

    manually removed event listener for the callback function

--- requestHandler.js
+++ requestHandler.js
@@ -30,6 +30,8 @@ stream.on('data', function(data) {
   console.log('Received data: ' + data);
 });
 
+stream.removeListener('data', callback);
+
 //  a callback is attached to the end event
 stream.on('end', function() {
   console.log('End of file has been reached');
